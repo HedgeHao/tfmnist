7cf0480e310ddedbb807cb1788901e9aa1e0e62e
joshchiu <joshchiu@lips-hci.com>
2022-01-26 16:59:23 +0800

    use class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -7,101 +8,149 @@
 #define INPUT_WIDTH 28
 #define INPUT_HEIGHT 28
 
-void NoOpDeallocator(void *data, size_t a, void *b) {}
-
-TF_Graph *Graph;
-TF_Session *Session;
-TF_SessionOptions *SessionOpts;
-TF_Output *Input;
-TF_Output *Output;
-TF_Status *Status;
 int NumInputs = 1;
 int NumOutputs = 1;
 
-int loadModel(const char *saved_model_dir, const char *tags)
+void NoOpDeallocator(void *data, size_t a, void *b) {}
+
+class TF_SavedModel
 {
-    Graph = TF_NewGraph();
-    Status = TF_NewStatus();
+public:
+    float *output;
 
-    SessionOpts = TF_NewSessionOptions();
-    TF_Buffer *RunOpts = NULL;
+    TF_SavedModel(const char *dir, const char *t,
+                  unsigned int numInDim, uint64_t *inDim,
+                  unsigned int dataSize, unsigned int numOutDim, uint64_t *outDim,
+                  std::vector<const char *> inputOpNames, std::vector<const char *> outputOpNames) : m_saved_model_dir(dir),
+                                                                                                     m_tags(t),
+                                                                                                     m_numInDim(numInDim),
+                                                                                                     m_dataSize(dataSize),
+                                                                                                     m_inputOpNames(inputOpNames),
+                                                                                                     m_outputOpNames(outputOpNames)
 
-    int ntags = 1;
-
-    Session = TF_LoadSessionFromSavedModel(SessionOpts, RunOpts, saved_model_dir, &tags, ntags, Graph, NULL, Status);
-    if (!TF_GetCode(Status) == TF_OK)
     {
-        printf("%s", TF_Message(Status));
-        return -1;
+        m_numInput = m_inputOpNames.size();
+        m_numOutput = m_outputOpNames.size();
+
+        m_inDim = new int64_t(numInDim);
+        int count = 1;
+        for (unsigned int i = 0; i < numOutDim; i++)
+            count *= outDim[i];
+        output = new float(count);
+
+        for (unsigned int i = 0; i < numInDim; i++)
+            m_inDim[i] = inDim[i];
     }
 
-    //****** Get input tensor
-    Input = (TF_Output *)malloc(sizeof(TF_Output) * NumInputs);
-
-    TF_Output t0 = {TF_GraphOperationByName(Graph, "serving_default_flatten_input"), 0};
-    if (t0.oper == NULL)
-        printf("ERROR: Failed TF_GraphOperationByName\n");
-    else
-        printf("TF_GraphOperationByName is OK\n");
-
-    Input[0] = t0;
-
-    //********* Get Output tensor
-    Output = (TF_Output *)malloc(sizeof(TF_Output) * NumOutputs);
-
-    TF_Output t2 = {TF_GraphOperationByName(Graph, "StatefulPartitionedCall"), 0};
-    if (t2.oper == NULL)
-        printf("ERROR: Failed TF_GraphOperationByName StatefulPartitionedCall\n");
-    else
-        printf("TF_GraphOperationByName StatefulPartitionedCall is OK\n");
-
-    Output[0] = t2;
-}
-
-void pipeline(cv::Mat img, float *output)
-{
-    //********* Allocate data for inputs & outputs
-    TF_Tensor **InputValues = (TF_Tensor **)malloc(sizeof(TF_Tensor *) * NumInputs);
-    TF_Tensor **OutputValues = (TF_Tensor **)malloc(sizeof(TF_Tensor *) * NumOutputs);
-
-    int ndims = 3;
-    int64_t dims[] = {1, INPUT_WIDTH, INPUT_HEIGHT};
-    int ndata = sizeof(float) * INPUT_WIDTH * INPUT_HEIGHT; // This is tricky, it number of bytes not number of element
-
-    TF_Tensor *in_tensor = TF_NewTensor(TF_FLOAT, dims, ndims, img.data, ndata, &NoOpDeallocator, 0);
-
-    if (in_tensor != NULL)
-        printf("TF_NewTensor is OK\n");
-    else
-        printf("ERROR: Failed TF_NewTensor\n");
-
-    InputValues[0] = in_tensor;
-
-    // Run the Session
-    TF_SessionRun(Session, NULL, Input, InputValues, NumInputs, Output, OutputValues, NumOutputs, NULL, 0, NULL, Status);
-    if (TF_GetCode(Status) == TF_OK)
+    ~TF_SavedModel()
     {
-        printf("Session is OK\n");
-    }
-    else
-    {
-        printf("%s", TF_Message(Status));
+        // Free memory
+        TF_DeleteGraph(Graph);
+        TF_DeleteSession(Session, Status);
+        TF_DeleteSessionOptions(SessionOpts);
+        TF_DeleteStatus(Status);
     }
 
-    // Free memory
-    TF_DeleteGraph(Graph);
-    TF_DeleteSession(Session, Status);
-    TF_DeleteSessionOptions(SessionOpts);
-    TF_DeleteStatus(Status);
+    int loadModel()
+    {
+        Graph = TF_NewGraph();
+        Status = TF_NewStatus();
 
-    float *buff = (float *)TF_TensorData(OutputValues[0]);
-    for (int i = 0; i < 10; i++)
-        output[i] = buff[i];
-}
+        SessionOpts = TF_NewSessionOptions();
+        TF_Buffer *RunOpts = NULL;
+
+        int ntags = 1;
+
+        Session = TF_LoadSessionFromSavedModel(SessionOpts, RunOpts, m_saved_model_dir, &m_tags, ntags, Graph, NULL, Status);
+        if (!TF_GetCode(Status) == TF_OK)
+        {
+            printf("%s", TF_Message(Status));
+            return -1;
+        }
+
+        //****** Get input tensor
+        Input = (TF_Output *)malloc(sizeof(TF_Output) * m_numInput);
+        for (int i = 0; i < m_inputOpNames.size(); i++)
+        {
+            TF_Output t0 = {TF_GraphOperationByName(Graph, m_inputOpNames[i]), 0};
+            if (t0.oper == NULL)
+            {
+                printf("ERROR: Failed TF_GraphOperationByName\n");
+                return -1;
+            }
+            Input[i] = t0;
+        }
+
+        //********* Get Output tensor
+        Output = (TF_Output *)malloc(sizeof(TF_Output) * m_numOutput);
+        for (int i = 0; i < m_outputOpNames.size(); i++)
+        {
+            TF_Output t2 = {TF_GraphOperationByName(Graph, m_outputOpNames[i]), 0};
+            if (t2.oper == NULL)
+            {
+                printf("ERROR: Failed TF_GraphOperationByName StatefulPartitionedCall\n");
+                return -2;
+            }
+            Output[i] = t2;
+        }
+
+        return 0;
+    }
+
+    int pipeline(cv::Mat img)
+    {
+        //********* Allocate data for inputs & outputs
+        TF_Tensor **InputValues = (TF_Tensor **)malloc(sizeof(TF_Tensor *) * m_numInput);
+        TF_Tensor **OutputValues = (TF_Tensor **)malloc(sizeof(TF_Tensor *) * m_numOutput);
+
+        TF_Tensor *in_tensor = TF_NewTensor(TF_FLOAT, m_inDim, m_numInDim, img.data, m_dataSize, &NoOpDeallocator, 0);
+
+        if (in_tensor == NULL)
+        {
+            printf("ERROR: Failed TF_NewTensor\n");
+            return -1;
+        }
+
+        InputValues[0] = in_tensor;
+
+        // Run the Session
+        TF_SessionRun(Session, NULL, Input, InputValues, m_numInput, Output, OutputValues, m_numOutput, NULL, 0, NULL, Status);
+        if (TF_GetCode(Status) != TF_OK)
+        {
+            printf("%s", TF_Message(Status));
+            return -2;
+        }
+
+        // TODO: how to parse output dimensions
+        float *buff = (float *)TF_TensorData(OutputValues[0]);
+        for (int i = 0; i < 10; i++)
+            output[i] = buff[i];
+
+        return 0;
+    }
+
+private:
+    TF_Graph *Graph;
+    TF_Session *Session;
+    TF_SessionOptions *SessionOpts;
+    TF_Output *Input;
+    TF_Output *Output;
+    TF_Status *Status;
+    int64_t *m_inDim;
+    int64_t *m_outDim;
+    uint64_t m_dataSize;
+    unsigned int m_numInDim;
+    unsigned int m_numInput;
+    unsigned int m_numOutDim;
+    unsigned int m_numOutput;
+    const char *m_saved_model_dir;
+    const char *m_tags;
+    std::vector<const char *> m_inputOpNames;
+    std::vector<const char *> m_outputOpNames;
+};
 
 void softmax(float *input, size_t size)
 {
-
     assert(0 <= size <= sizeof(input) / sizeof(double));
 
     int i;
@@ -109,31 +158,23 @@ void softmax(float *input, size_t size)
 
     m = -INFINITY;
     for (i = 0; i < size; ++i)
-    {
         if (m < input[i])
-        {
             m = input[i];
-        }
-    }
 
     sum = 0.0;
     for (i = 0; i < size; ++i)
-    {
         sum += exp(input[i] - m);
-    }
 
     constant = m + log(sum);
     for (i = 0; i < size; ++i)
-    {
         input[i] = exp(input[i] - constant);
-    }
 }
 
 int main()
 {
     printf("+++\n");
 
-    char *file = "../images/three_hand.png";
+    const char *file = "../images/three_hand.png";
     cv::Mat img = cv::imread(file, 1);
 
     // cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
@@ -145,17 +186,33 @@ int main()
     // cv::imshow("Preview", img);
     // cv::waitKey(0);
 
-    loadModel("mnist_saved_model", "serve");
-    float *output = new float[10];
-    pipeline(img, output);
-    softmax(output, 10);
+    uint64_t in[] = {1, INPUT_WIDTH, INPUT_HEIGHT};
+    uint64_t out[] = {1, 10};
+    unsigned int dataSize = sizeof(float) * INPUT_WIDTH * INPUT_HEIGHT;
+    std::vector<const char *> inputOpNames = {"serving_default_flatten_input"};
+    std::vector<const char *> outputOpNames = {"StatefulPartitionedCall"};
+    TF_SavedModel *tfModel = new TF_SavedModel("mnist_saved_model", "serve", 3, in, dataSize, 2, out, inputOpNames, outputOpNames);
+
+    int ret = 0;
+    tfModel->loadModel();
+    if (ret > 0)
+    {
+        printf("Load model failed:%d\n", ret);
+        return ret;
+    }
+
+    tfModel->pipeline(img);
+    if (ret > 0)
+    {
+        printf("Pipeline failed:%d\n", ret);
+        return ret;
+    }
+    softmax(tfModel->output, 10);
 
     int max = 0;
     for (int i = 0; i < 10; i++)
-    {
-        if (output[i] > output[max])
+        if (tfModel->output[i] > tfModel->output[max])
             max = i;
-    }
     printf("result: %d\n", max);
 
     printf("---\n");
